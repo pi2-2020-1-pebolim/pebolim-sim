ba0957078f56c55c8a72342513f5d429eb6132d5
Marco Antonio <markinlimac@gmail.com>
2020-09-21 00:49:45 -0300

    feat: adding engines script

---
+++ Assets/src/scripts/JsonGenerate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonGenerate : MonoBehaviour
+{
+    public Dictionary<string, Dictionary<string, int>> json;
+    void MovementGenerator()
+    {
+        List<Dictionary<string, int>> engine = new List<Dictionary<string, int>>();
+        this.json = new Dictionary<string, Dictionary<string, int>>();
+        
+        for (int i = 0; i < 4; i++)
+        {
+            Dictionary<string, int> temp = new Dictionary<string, int>();
+            temp.Add("rotacao", Random.Range(0, 361));
+            temp.Add("movimento", Random.Range(-7, 7));
+            engine.Add(temp);
+            this.json.Add("Motor"+(i+1), engine[i]);
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementGenerator();
+        
+    }
+}
---
+++ Assets/src/scripts/MotorMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotorMove : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject decisionServer = GameObject.Find("DecisionServer");
+        JsonGenerate decisionServerScript = decisionServer.GetComponent<JsonGenerate>();
+        
+        if (gameObject.name == "Rod1")
+        {
+            transform.Translate(0, decisionServerScript.json["Motor1"]["movimento"], 0);
+            transform.Rotate(0, 0, 0);
+        } else if (gameObject.name == "Rod2")
+        {
+            transform.Translate(0, decisionServerScript.json["Motor2"]["movimento"], 0);
+            transform.Rotate(0, 0, 0);
+        } else if (gameObject.name == "Rod4")
+        {
+            transform.Translate(0, decisionServerScript.json["Motor3"]["movimento"], 0);
+            transform.Rotate(0, 0, 0);
+        } else if (gameObject.name == "Rod6")
+        {
+            transform.Translate(0, decisionServerScript.json["Motor4"]["movimento"], 0);
+            transform.Rotate(0, 0, 0);
+        }
+    }
+}
