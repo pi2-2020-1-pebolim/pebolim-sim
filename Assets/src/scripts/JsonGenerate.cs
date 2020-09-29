ab81e87293fb0f5282aed1a1e1ec6944f8c6caed
Daniel Maike <danmke@hotmail.com>
2020-09-29 20:08:11 -0300

    feat: improve code
    Co-authored-by: markinlimac <markinlimac@gmail.com>
    Co-authored-by: guilherme1guy <guilhermeguy349@gmail.com>

--- Assets/src/scripts/JsonGenerate.cs
+++ Assets/src/scripts/JsonGenerate.cs
@@ -4,32 +4,39 @@ using UnityEngine;
 
 public class JsonGenerate : MonoBehaviour
 {
+    
+    [SerializeField]
+    private Vector2Int positionRange;
+    
+    [SerializeField]
+    private Vector2Int rotationRange;
+    
+    [SerializeField]
+    private float updateTime;
     public Dictionary<string, Dictionary<string, int>> json;
     void MovementGenerator()
     {
         List<Dictionary<string, int>> engine = new List<Dictionary<string, int>>();
-        this.json = new Dictionary<string, Dictionary<string, int>>();
+        
+        var tempJson = new Dictionary<string, Dictionary<string, int>>();
         
         for (int i = 0; i < 4; i++)
         {
             Dictionary<string, int> temp = new Dictionary<string, int>();
-            temp.Add("rotation", Random.Range(-10000, 10000));
-            temp.Add("position", Random.Range(-10000, 10000));
+            temp.Add("rotation", Random.Range(rotationRange.x, rotationRange.y));
+            temp.Add("position", Random.Range(positionRange.x, positionRange.y));
             engine.Add(temp);
-            this.json.Add("laneID"+(i+1), engine[i]);
+            tempJson.Add("laneID"+(i+1), engine[i]);
         }
+
+        this.json = tempJson;
     }
     
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        InvokeRepeating("MovementGenerator", 0, updateTime);
 
-    // Update is called once per frame
-    void Update()
-    {
-        MovementGenerator();
-        
     }
+    
 }
--- Assets/src/scripts/MotorMove.cs
+++ Assets/src/scripts/MotorMove.cs
@@ -6,13 +6,20 @@ using UnityEngineInternal;
 public class MotorMove : MonoBehaviour
 {    
     
-    private float limitTranslateRod1 = 10.5f;
-    private float limitTranslateRod2 = 17.0f;
-    private float limitTranslateRod4 = 3.5f;
-    private float limitTranslateRod6 = 13.5f; 
+    // private float limitTranslateRod1 = 10.5f;
+    // private float limitTranslateRod2 = 17.0f;
+    // private float limitTranslateRod4 = 3.5f;
+    // private float limitTranslateRod6 = 13.5f; 
+    
+    [SerializeField]
+    private float limitTranslate;
+    
+    [SerializeField]
+    private string laneId;
+
     private JsonGenerate decisionServerScript;
 
-    private float calculateLimit(float positionZ, float movement, float limitTranslate)
+    private float calculateLimit(float positionZ, float movement)
     {
         if (positionZ + movement > limitTranslate)
         {
@@ -36,31 +43,9 @@ public class MotorMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.name == "Rod1")
-        {
-            float movement = calculateLimit(gameObject.transform.position.z, decisionServerScript.json["laneID1"]["position"], limitTranslateRod1);
-            float rotacao = decisionServerScript.json["laneID1"]["rotation"];
-            transform.Translate(0, movement * Time.deltaTime, 0);
-            // transform.Rotate(0, 0, 0);
-            transform.RotateAround(gameObject.transform.position, Vector3.forward, rotacao * Time.deltaTime);
-        } else if (gameObject.name == "Rod2")
-        {
-            float movement = calculateLimit(gameObject.transform.position.z, decisionServerScript.json["laneID2"]["position"], limitTranslateRod2);
-            float rotacao = decisionServerScript.json["laneID2"]["rotation"];
-            transform.Translate(0, movement * Time.deltaTime, 0);
-            transform.RotateAround(gameObject.transform.position, Vector3.forward, rotacao * Time.deltaTime);
-        } else if (gameObject.name == "Rod4")
-        {
-            float movement = calculateLimit(gameObject.transform.position.z, decisionServerScript.json["laneID3"]["position"], limitTranslateRod4);
-            float rotacao = decisionServerScript.json["laneID3"]["rotation"];
-            transform.Translate(0, movement * Time.deltaTime, 0);
-            transform.RotateAround(gameObject.transform.position, Vector3.forward, rotacao);
-        } else if (gameObject.name == "Rod6")
-        {
-            float movement = calculateLimit(gameObject.transform.position.z, decisionServerScript.json["laneID4"]["position"], limitTranslateRod6);
-            float rotacao = decisionServerScript.json["laneID4"]["rotation"];
-            transform.Translate(0, movement * Time.deltaTime, 0);
-            transform.RotateAround(gameObject.transform.position, Vector3.forward, rotacao * Time.deltaTime);
-        }
+        float movement = calculateLimit(gameObject.transform.position.z, decisionServerScript.json[laneId]["position"]);
+        float rotacao = decisionServerScript.json[laneId]["rotation"];
+        transform.Translate(0, movement * Time.deltaTime, 0);
+        transform.RotateAround(gameObject.transform.position, Vector3.forward, rotacao * Time.deltaTime);
     }
 }
