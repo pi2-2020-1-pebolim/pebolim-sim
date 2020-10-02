308b9f366148940fe277c25aba3691abff6a92db
Guilherme Guy <guilhermeguy349@gmail.com>
2020-10-02 19:33:47 -0300

    feat: adds initial player kick

---
+++ Assets/src/testing/ConstantRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantRotation : MonoBehaviour
+{
+
+    [SerializeField]
+    float constanteAngles;
+
+    float lastRotationStep;
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastRotationStep = constanteAngles * Time.deltaTime;
+        transform.RotateAround(transform.position, Vector3.forward, lastRotationStep);    
+    }
+
+    public float GetLastRotationStep() {
+        return lastRotationStep;
+    }
+}
---
+++ Assets/src/testing/DebugVelocity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugVelocity : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Debug.DrawLine(transform.position, (transform.position + rigidbody.velocity), Color.magenta);
+    }
+
+    void OnCollisionEnter(Collision collision) {
+
+        Debug.DrawLine(collision.GetContact(0).point, transform.position, Color.cyan, 1);
+    }
+
+    
+    
+}
